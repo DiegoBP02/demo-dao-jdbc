ced1b6f01652020d888460108f6e308f5658845b
Diego Batista Pereira <diegobpcelular@gmail.com>
2023-05-03 15:46:08 -0300

    department insert implementation

--- src/application/Main2.java
+++ src/application/Main2.java
@@ -23,6 +23,11 @@ public class Main2 {
             System.out.println(d);
         }
 
+        System.out.println("\n=== TEST 3: insert ===");
+        Department newDepartment = new Department(null, "Music");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New id: " + newDepartment.getId());
+
         sc.close();
     }
 }
